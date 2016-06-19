20c7e8ecd095458746371c288a1d7dc316fca1e5
Dmitry Svetlakov <mail@ae0n.name>
2016-06-19 23:20:38 +0500

    [+] added underscorejs to bower dependency list [+] added open sans as base font [+] added bills viewer on place page

--- app/components/placeReview.js
+++ app/components/placeReview.js
@@ -1,7 +1,7 @@
 /**
  * Created by aeon on 19/06/16.
  */
-Application.controller("PlaceReviewController", ["$scope", "$http", "$state" , "place" , "services", "bills",
+Application.controller("PlaceReviewController", ["$scope", "$http", "$state", "place", "services", "bills",
     function ($scope, $http, $state, placeData, servicesData, billsData) {
 
         $scope.place = placeData.data.place;
@@ -9,6 +9,29 @@ Application.controller("PlaceReviewController", ["$scope", "$http", "$state" , "
         $scope.services = servicesData.data.services;
 
         $scope.bills = billsData.data.bills;
+
+        $scope.monthlyGroupedBills = [];
+
+        function transformBills() {
+            var groupedBills = _.groupBy($scope.bills, function (b) {
+                var created = new Date(b.created);
+                return (created.getMonth() + 1) + "/" + created.getFullYear();
+            });
+
+            var months = _.keys(groupedBills);
+
+            _.forEach(months, function (m) {
+                var monthlyBills = groupedBills[m];
+                var billsForService = _.groupBy(monthlyBills, "serviceId");
+                groupedBills[m] = _.map($scope.services, function (service) {
+                    return billsForService[service.id] || [];
+                })
+            });
+
+            $scope.monthlyGroupedBills = groupedBills
+        }
+
+        transformBills();
 
     }
 ]);
