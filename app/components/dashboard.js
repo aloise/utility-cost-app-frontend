a73a57ea4ba78956f69c1fad2e487b981653a099
aloise <aloise@aloise.name>
2016-06-19 19:40:45 +0300

    [+] dashboard controller - display stats - amounts

--- app/components/dashboard.js
+++ app/components/dashboard.js
@@ -4,12 +4,14 @@ Application.controller("DashboardController", ["$scope", "$http", "$state" , "pl
             $scope.places = placesData.data.places;
 
             $scope.stats = globalStatsData.data.stats;
-            /* {
+            /*
+            {
                 placesCount: 0,
                 totalServices: 0,
-                totalPaid:0,
-                totalDebt:0
-            };*/
+                totalPaid:[ { currency : "USD", amount: 123 }, ... ],
+                totalDebt:[ { currency : "USD", amount: 123 }, ... ]
+            };
+            */
 
         }
 ]);
