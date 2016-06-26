97a377e5a5d4d339216cabbe3deb5c1adc363f78
Dmitry Svetlakov <mail@ae0n.name>
2016-06-26 23:43:22 +0500

    [+] service edit page

--- app/components/serviceEdit.js
+++ app/components/serviceEdit.js
@@ -4,6 +4,28 @@
 Application.controller("ServiceEditController", ["$scope", "$http", "$state", "$stateParams", "$timeout", "place", "services", "settings",
     function ($scope, $http, $state, $stateParams, $timeout, placeData, servicesData, settings) {
 
-        console.log("ServiceEditController", placeData);
+        $scope.place = placeData.data.place;
+
+        $scope.services = servicesData.data.services;
+
+        function transformServices(){
+            $scope.services = _.map($scope.services, function (service) {
+                    if (service.serviceRate.rateData.ProgressiveRateData) {
+                        var previousRate = null;
+                        service.zippedRates = _.chain(service.serviceRate.rateData.ProgressiveRateData.rates).zip(service.serviceRate.rateData.ProgressiveRateData.prices).map(function(z){
+                            var result = {rate:z[0], price:z[1]};
+                            if(previousRate){
+                                result.previousRate = previousRate;
+                            }
+                            previousRate = _.omit(result,"previousRate");
+                            return result;
+                        }).value();
+                    }
+                    return service;
+                }
+            );
+        }
+
+        transformServices();
 
     }]);
